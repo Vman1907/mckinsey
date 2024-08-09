60c08b8428de2efd428549a7cbac6d3fcae8ee8b
Varshmaan Sonkar <varshmaan.sonkar@gmail.com>
2024-08-09 21:27:31 +0530

    chore: Update npm dependencies and add logout route

--- server/src/routes/questionRoutes.ts
+++ server/src/routes/questionRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { createQuestion, getQuestions } from '../controllers/questionController';
-import authMiddleware from '../middleware/authMiddleware';
+import { protect } from '../middleware/authMiddleware';
 import roleMiddleware from '../middleware/roleMiddleware';
 
 const router = Router();
 
-router.post('/questions', authMiddleware, roleMiddleware('Admin'), createQuestion);
-router.get('/questions', authMiddleware, getQuestions);
+router.post('/questions', protect, roleMiddleware('Admin'), createQuestion);
+router.get('/questions', protect, getQuestions);
 
 export default router;
